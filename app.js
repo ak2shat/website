d8be3d1a09175d9e95a7963a7cfb1c29d5cee849
Akshat <akshatkool2@gmail.com>
2021-11-29 13:21:33 +0530

    first commit respo

---
+++ app.js
@@ -0,0 +1,25 @@
+let mynav = document.getElementById('nvbar');
+let nvlinks = document.getElementsByClassName('nav-link');
+let logo = document.getElementsByClassName('txt-big');
+
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        mynav.classList.add('sticky-top');
+        
+        mynav.classList.add('blue');
+        for(let i=0;i<nvlinks.length;i++){
+            nvlinks[i].classList.add('text-white');
+        }
+        logo[0].classList.add('text-white');
+    }
+    else{
+        mynav.classList.remove('sticky-top');
+        
+        mynav.classList.remove('blue');
+        for(let i=0;i<nvlinks.length;i++){
+            nvlinks[i].classList.remove('text-white');
+        }
+        logo[0].classList.remove('text-white');
+    }
+
+}
